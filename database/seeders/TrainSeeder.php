e1d86115865f4a0e5a0d9dcda57679a85c002101
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-04-13 14:54:43 +0200

    EDIT faker seeder to make db

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TrainSeeder extends Seeder
+
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+        $azienda  = ['Trenitalia', 'Italo'];
+
+        for ($i = 0; $i < 100; $i++) {
+
+            $new_train = new Train();
+            $new_train->azienda = $faker->randomElement($azienda);
+            $new_train->stazione_di_partenza = $faker->city();
+            $new_train->stazione_di_arrivo = $faker->city();
+            $new_train->orario_di_partenza = $faker->time();
+            $new_train->orario_di_arrivo = $faker->time();
+            $new_train->codice_treno = $faker->numberBetween(1, 127);
+            $new_train->numero_carrozze = $faker->numberBetween(1, 127);
+            $new_train->in_orario = $faker->numberBetween(0, 1);
+            $new_train->cancellato = $faker->numberBetween(0, 1);
+
+            $new_train->save();
+        }
+    }
+}
